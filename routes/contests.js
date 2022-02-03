4bfb8fd1e46477eb184f464a8701694836b6af72
AN-Akhand <1805089@ugrad.cse.buet.ac.bd>
2022-02-04 02:44:32 +0600

    Added basic problem creation

--- routes/contests.js
+++ routes/contests.js
@@ -3,13 +3,14 @@ import auth from '../Middleware/auth.js';
 import { executeQuery } from '../DB/executequery.js';
 import moment from 'moment';
 import oracledb from 'oracledb';
+import fs from 'fs';
 
 const router = express.Router();
 router.use(express.json());
 
 router.get('/', async (req, res) => {
     console.log("list of contests");
-    res.send("contests");
+    res.json({bleh: "asdsd"});
 });
 
 router.post('/create', auth, async (req, res) => {
@@ -27,6 +28,12 @@ router.post('/create', auth, async (req, res) => {
                     END;`;
         let result = await executeQuery(query, {start_time, end_time, handle, title, id: { dir: oracledb.BIND_OUT, type: oracledb.NUMBER }});
         let contest_id = result.outBinds.id;
+        let dir = "contests/" + contest_id;
+        if(!fs.existsSync(dir)){
+            fs.mkdirSync(dir, (err, file)=>{
+                if(err) throw err;
+            });
+        }
         query = `BEGIN
                     insert_blog(:handle, :data, :id);
                 END;`;
@@ -34,7 +41,7 @@ router.post('/create', auth, async (req, res) => {
         let blog_id = result.outBinds.id;
         query = `INSERT INTO announcements(blog_id, contest_id) VALUES(:blog_id, :contest_id)`;
         result = await executeQuery(query, {blog_id, contest_id});
-        res.sendStatus(200)
+        res.json({contestId: contest_id});
     }catch(err){
         res.sendStatus(500);
         console.error(err);
--- routes/problems.js
+++ routes/problems.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import auth from '../Middleware/auth.js';
 import { executeQuery } from '../DB/executequery.js';
+import fs from 'fs';
 
 const router = express.Router();
 router.use(express.json());
@@ -8,6 +9,66 @@ router.use(express.json());
 router.get('/', async (req, res) => {
     console.log("list of problems");
     res.send("problems");
+})
+
+router.post('/create', auth, async(req, res) => {
+    try{
+        let problem = req.body;
+        let dir = "contests/" + problem.contestId + "/" + problem.problemNo;
+        if(!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
+            fs.mkdirSync(dir + "/inputs");
+            fs.mkdirSync(dir + "/outputs");
+            fs.mkdirSync(dir + "/sampleInputs");
+            fs.mkdirSync(dir + "/sampleOutputs");
+        }
+        fs.writeFile(dir + "/statement.md", problem.statement, (err, file)=>{
+            if(err) throw err;
+        })
+        problem.sampleInputs.forEach(file => {
+            fs.writeFile(dir + "/sampleInputs/" + file.fileName, file.data, (err, file) =>{
+                if(err) throw err;
+            })
+        });
+        problem.sampleOutputs.forEach(file => {
+            fs.writeFile(dir + "/sampleOutputs/" + file.fileName, file.data, (err, file) =>{
+                if(err) throw err;
+            })
+        });
+        problem.inputs.forEach(file => {
+            fs.writeFile(dir + "/inputs/" + file.fileName, file.data, (err, file) =>{
+                if(err) throw err;
+            })
+        });
+        problem.outputs.forEach(file => {
+            fs.writeFile(dir + "/outputs/" + file.fileName, file.data, (err, file) =>{
+                if(err) throw err;
+            })
+        });
+
+        let s = "";
+        problem.category.forEach(c=>{
+            s = s + c + '/';
+        })
+        s = s.slice(0, -1);
+
+        let query = `INSERT INTO problems(problem_id, contest_id, name, time_limit, memory_limit, difficulty, handle, categories) VALUES(:problemId, :contestId, :name, :timeLimit, :memoryLimit, :difficulty, :handle, :categories)`;
+        let bind = {
+            problemId: problem.problemNo,
+            contestId: problem.contestId,
+            name: problem.name,
+            timeLimit: problem.timeLimit,
+            memoryLimit: problem.memoryLimit,
+            difficulty: problem.difficulty,
+            handle: res.locals.handle,
+            categories: s
+        } 
+        let result = await executeQuery(query, bind);
+        res.sendStatus(200);
+    }catch(err){
+        res.send(500);
+        console.log(err);
+    }
 })
 
 export default router;
